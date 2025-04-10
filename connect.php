fa66fc8f9d471c0cce2136feb757b99c76b42728
mohamedpr0o4591 <ev9ry.24hours@gmail.com>
2025-04-10 19:17:32 +0200

    update

--- connect.php
+++ connect.php
@@ -11,18 +11,79 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit();
 }
 
-$dns = "mysql:host=localhost;dbname=fastcreditcards_db";
-$user = 'root';
-$pass = '';
-$option = array(
-    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"
-);
+// $dns = "mysql:host=localhost;dbname=fastcreditcards_db";
+// $user = 'root';
+// $pass = '';
+// $option = array(
+//     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"
+// );
+
+// try {
+//     $con = new PDO($dns, $user, $pass, $option);
+//     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+//     include "actions.php";
+// } catch (PDOException $e) {
+//     echo $e->getMessage();
+// }
+
+$host = "localhost";
+$dbname = "fastcreditcards_db";
+$user = "root";
+$pass = "";
 
 try {
-    $con = new PDO($dns, $user, $pass, $option);
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn = new PDO("mysql:host=$host", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    include "actions.php";
+    $conn->exec("CREATE DATABASE IF NOT EXISTS $dbname CHARACTER SET uft8mb4 COLLATE utf8mb4_unicode_ci");
+
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = [];
+
+    $sql[] =  "CREATE TABLE IF NOT EXISTS `users` (
+        `token` VARCHAR(255) NOT NULL PRIMARY KEY ,
+        `username` VARCHAR(255) NOT NULL ,
+        `email` VARCHAR(255) NOT NULL ,
+        `password` VARCHAR(255) NOT NULL ,
+        `fullName` VARCHAR(255) NOT NULL ,
+        `gender` VARCHAR(255) NOT NULL ,
+        `birthDate` VARCHAR(255) NOT NULL ,
+        `country` VARCHAR(255) NOT NULL ,
+        `verified` VARCHAR(255) NULL,
+    ) ";
+
+    $sql[] = "CREATE TABLE IF NOT EXISTS `points_user` (
+        `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `points_user` VARCHAR(255) NOT NULL,
+        `points_earn` VARCHAR(255) NULL DEFAULT '0',
+        `points_deposit` VARCHAR(255) NULL DEFAULT '0',
+        `points_affiliate` VARCHAR(255) NULL DEFAULT '0',
+        FOREIGN KEY (`points_user`) REFERENCES `users` (`token`) ON DELETE CASCADE ON UPDATE CASCADE
+    )" ;
+
+    $sql[] = "CREATE TABLE IF NOT EXISTS `more_user_info` (
+        `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `user_token` VARCHAR(255)  NULL,
+        FOREIGN KEY (`user_token`) REFERENCES `users` (`token`) ON DELETE CASCADE ON UPDATE CASCADE
+    )" ;
+
+    $sql[] = "CREATE TABLE IF NOT EXISTS `inviteuser` (
+        `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `i_user` VARCHAR(255)  NULL,
+        `i_userInvited` VARCHAR(255)  NULL,
+        FOREIGN KEY (`i_user`) REFERENCES `users` (`token`) ON DELETE CASCADE ON UPDATE CASCADE
+    )" ;
+
+    if (!empty($sql)) {
+        foreach ($sql as $query) {
+            $conn->exec($query);
+        }
+    }
+
+    include("../actions.php");
 } catch (PDOException $e) {
-    echo $e->getMessage();
+    echo "" . $e->getMessage();
 }
